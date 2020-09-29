04e1bd596338945addec51d45347bb388ca7cf1e
Chloe Steffens <caforonda103@yahoo.com>
2020-09-29 06:30:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function wakeDog(dogName, dogBreed) {
-  console.log("Wake ${dogName} the poodle");
-  console.log("Leash Byron the poodle");
-  console.log("Walk to the park with Byron the poodle");
-  console.log("Throw the frisbee for Byron the poodle");
-  console.log("Walk home with Byron the poodle");
-  console.log("Unleash Byron the poodle");
+  console.log(`Wake ${dogName} the ${dogBreed}`);
+  console.log(`Leash ${dogName} the poodle`);
+  console.log("Walk to the park with ${dogName} the poodle");
+  console.log("Throw the frisbee for ${dogName} the poodle");
+  console.log("Walk home with ${dogName} the poodle");
+  console.log("Unleash ${dogName} the poodle");
 }
