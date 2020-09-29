1c6071e41019295141752fd56343ca27805ed714
Chloe Steffens <caforonda103@yahoo.com>
2020-09-29 06:27:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 function wakeDog() {
-  
+  console.log("Wake Byron the poodle");
+  console.log("Leash Byron the poodle");
+  console.log("Walk to the park with Byron the poodle");
+  console.log("Throw the frisbee for Byron the poodle");
+  console.log("Walk home with Byron the poodle");
+  console.log("Unleash Byron the poodle");
 }
